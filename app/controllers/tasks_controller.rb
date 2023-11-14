c85a377aa9d96d3b44f737ec1e7e6314311d1006
Carlos Coelho <carlos.neto.coelho@outlook.pt>
2023-11-14 15:31:36 +0000

    implemented index show new create edit and update

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,44 @@
+class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params) #create new restaurant with params
+    @task.save #save it
+    redirect_to tasks_path #redirect to index
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params) #update
+    redirect_to task_path(@task) #redirect to show
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    ## whitelisting params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+end
